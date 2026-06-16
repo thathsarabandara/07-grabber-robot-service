9e743af9b26a0b462b54a3e1fedb6acb2ca67111
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-06-16 15:42:17 +0530

    feat: add redis configurations to the config files

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,9 @@ class Settings(BaseSettings):
     MQTT_USERNAME: str | None = None
     MQTT_PASSWORD: str | None = None
     
+    # Redis Settings
+    REDIS_URL: str = "redis://redis:6379/0"
+    
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 settings = Settings()
